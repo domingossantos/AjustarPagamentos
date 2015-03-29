7fd4ced899cc889b87e465b4aaa901d020d514c4
Domingos Santos <domsantos@gmail.com>
2015-03-29 17:25:59 -0300

    conclusão da função de apagar pagamento

--- AjustarPagamentos/Form1.Designer.cs
+++ AjustarPagamentos/Form1.Designer.cs
@@ -28,20 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtFim = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtInicio = new System.Windows.Forms.DateTimePicker();
+            this.txCodigo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.btApagar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btApagar);
+            this.panel1.Controls.Add(this.btBuscar);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.dtFim);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.dtInicio);
+            this.panel1.Controls.Add(this.txCodigo);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(504, 61);
+            this.panel1.TabIndex = 0;
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.Location = new System.Drawing.Point(316, 22);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btBuscar.TabIndex = 6;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            this.btBuscar.Click += new System.EventHandler(this.btBuscar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(206, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Data Final";
+            // 
+            // dtFim
+            // 
+            this.dtFim.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFim.Location = new System.Drawing.Point(209, 25);
+            this.dtFim.Name = "dtFim";
+            this.dtFim.Size = new System.Drawing.Size(89, 20);
+            this.dtFim.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Data Inicial";
+            // 
+            // dtInicio
+            // 
+            this.dtInicio.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtInicio.Location = new System.Drawing.Point(101, 25);
+            this.dtInicio.Name = "dtInicio";
+            this.dtInicio.Size = new System.Drawing.Size(87, 20);
+            this.dtInicio.TabIndex = 2;
+            // 
+            // txCodigo
+            // 
+            this.txCodigo.Location = new System.Drawing.Point(15, 25);
+            this.txCodigo.Name = "txCodigo";
+            this.txCodigo.Size = new System.Drawing.Size(67, 20);
+            this.txCodigo.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.Location = new System.Drawing.Point(0, 61);
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.Size = new System.Drawing.Size(504, 322);
+            this.grid.TabIndex = 1;
+            // 
+            // btApagar
+            // 
+            this.btApagar.Location = new System.Drawing.Point(397, 23);
+            this.btApagar.Name = "btApagar";
+            this.btApagar.Size = new System.Drawing.Size(75, 23);
+            this.btApagar.TabIndex = 7;
+            this.btApagar.Text = "Apagar";
+            this.btApagar.UseVisualStyleBackColor = true;
+            this.btApagar.Click += new System.EventHandler(this.btApagar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(422, 303);
+            this.ClientSize = new System.Drawing.Size(504, 383);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Ajustar Pagamentos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.DateTimePicker dtInicio;
+        private System.Windows.Forms.TextBox txCodigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtFim;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btApagar;
     }
 }
 
--- AjustarPagamentos/Form1.cs
+++ AjustarPagamentos/Form1.cs
@@ -11,9 +11,96 @@ namespace AjustarPagamentos
 {
     public partial class Form1 : Form
     {
+        ConexaoDB conexao;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conexao = new ConexaoDB(Properties.Settings.Default.connACP);
+        }
+
+        private void btBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String sql = "select dt_mesRefContribuicao mes,dt_anoRefContribuicao ano " +
+                             " ,cd_tipoContribuicao tipo , vl_ContribuicaoVencimento valor from tbContribuicoes ";
+
+                sql += " where cd_cadastro = " + txCodigo.Text;
+
+                sql += " and dt_Contribuicao between '" + formataData(dtInicio.Value.ToShortDateString()) + "' ";
+                sql += " and '" + formataData(dtFim.Value.ToShortDateString()) + "'";
+                sql += " order by dt_anoRefContribuicao, dt_mesRefContribuicao	  ";
+
+                conexao.abreBanco();
+
+                grid.DataSource = conexao.retornarDataSet(sql);
+
+            }
+            finally {
+                conexao.fechaBanco();
+            }
+
+        }
+
+        private String formataData(String data) {
+            String[] dataV = data.Split('/');
+            data = dataV[2]+"-"+dataV[1]+"-"+dataV[0];
+
+            return data;
+        }
+
+        private void btApagar_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result1 = MessageBox.Show("Você deseja realmente apagar \no(s) pagamento(s) selecionado(s)?",
+        "Atenção",
+        MessageBoxButtons.YesNo);
+
+
+            if (result1 == System.Windows.Forms.DialogResult.Yes)
+            {
+
+                foreach (DataGridViewRow linha in grid.SelectedRows)
+                {
+
+                    apagarPagamento(txCodigo.Text,
+                                    linha.Cells[2].Value.ToString(),
+                                    linha.Cells[0].Value.ToString(),
+                                    linha.Cells[1].Value.ToString());
+
+                   
+                }
+
+                MessageBox.Show("Pagamento(s) apagado(s)!");
+            }
+        }
+
+        private void apagarPagamento(String codigo, String tipo, String mes, String ano) {
+            try
+            {
+                conexao.abreBanco();
+
+                String sql = "delete from tbContribuicoes where cd_cadastro =" + codigo +
+                             " and cd_tipoContribuicao = " + tipo +
+                             " and dt_mesRefContribuicao = " + mes +
+                             " and dt_anoRefContribuicao = " + ano;
+                
+                conexao.executaSemRetorno(sql);
+
+                sql = "delete from tbPagtoContribuicao where cd_cadastro =" + codigo +
+                             " and cd_tipoContribuicao = " + tipo +
+                             " and dt_mesRefContribuicao = " + mes +
+                             " and dt_anoRefContribuicao = " + ano;
+
+                conexao.executaSemRetorno(sql);
+            }
+            finally {
+                conexao.fechaBanco();
+            }
         }
     }
 }
